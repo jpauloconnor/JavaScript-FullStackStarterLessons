bc9b965afa29749c398ca83727f4e68a2fb40a35
Paul O'Connor <jamespauloconnor@gmail.com>
2018-03-23 23:34:00 -0400

    Server get by id and user

--- completedCode/server/controllers/authtestcontroller.js
+++ completedCode/server/controllers/authtestcontroller.js
@@ -41,49 +41,22 @@ router.post('/create', function (req, res) {
 		);
 });
 
+router.get('/:id', function(req, res) {
+	var data = req.params.id;
+	var userid = req.user.id;
 
+	AuthTestModel
+		.findOne({
+			where: { id: data, owner: userid }
+		}).then(
+			function findOneSuccess(data) {
+				res.json(data);
+			},
+			function findOneError(err) {
+				res.send(500, err.message);
+			}
+		);
+});
 
-
-// router.get('/id', function(req, res) {
-// 	var data = req.params.id;
-// 	var userid = req.user.id;
-
-// 	AuthTestModel
-// 		.findOne({
-// 			where: { id: data, owner: userid }
-// 		}).then(
-// 			function getSuccess(updateData) {
-// 				res.json(updateData);
-// 			},
-
-// 			function getError(err) {
-// 				res.send(500, err.message);
-// 			}
-// 		);
-// });
-
-
-
-
-
-// router.post('/one', function(req, res) {
-
-//     var authTestData = req.body.authtestdata.item;
-// 	var user = req.user;
-
-//   	AuthTestModel
-// 	    .create({
-// 			authtestdata: authTestData,
-// 			owner: user.id
-// 	    })
-// 	    .then(
-// 	    	function createSuccess(data) {
-// 	    		res.json(data);
-// 	    	},
-// 		    function createError(err) {
-// 		        res.send(500, err.message);
-// 		    }
-// 	    );
-// });
 
 module.exports = router;
