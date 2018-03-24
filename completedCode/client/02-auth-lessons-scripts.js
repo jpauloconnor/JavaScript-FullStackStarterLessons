079f29b2e4731df516edee37f9fe506e7c0af087
Paul O'Connor <jamespauloconnor@gmail.com>
2018-03-23 23:09:34 -0400

    Post from client working

--- completedCode/client/02-auth-lessons-scripts.js
+++ completedCode/client/02-auth-lessons-scripts.js
@@ -13,8 +13,37 @@ function fetchFromAuthRouteOne () {
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
-		  'Authorization': accessToken,
+		  'Authorization': accessToken
 		}
+	})
+	.then(response => {
+		return response.json();
+	})
+	.then(data => {
+		console.log(data)
+	})   
+}
+
+
+/***************************************
+ * FETCH/POST to Auth/Create
+*************************************/
+
+function postToAuthRouteCreate() {
+	const fetch_url = `http://localhost:3000/authtest/create`
+	const accessToken = localStorage.getItem('SessionToken')
+
+    let authTestDataInput = document.getElementById('authTestData').value;
+
+	let authInputData = {authtestdata : { item: authTestDataInput}};
+	const response = fetch(fetch_url, {
+		method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+		  'Authorization': accessToken
+		},
+		body: JSON.stringify(authInputData)
 	})
 	.then(response => {
 		return response.json();
