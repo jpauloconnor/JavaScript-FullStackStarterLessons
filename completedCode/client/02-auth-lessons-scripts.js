4a640b0c1bc43a0b700578ac1935372b94a1c3c6
Paul O'Connor <jamespauloconnor@gmail.com>
2018-03-24 00:06:18 -0400

    Getting item by id

--- completedCode/client/02-auth-lessons-scripts.js
+++ completedCode/client/02-auth-lessons-scripts.js
@@ -52,3 +52,35 @@ function postToAuthRouteCreate() {
 		console.log(data)
 	})   
 }
+
+
+/***************************************
+ * FETCH/GET ITEM BY USER
+*************************************/
+
+function getOneByUser() {
+	const fetch_url = `http://localhost:3000/authtest/10`
+	const accessToken = localStorage.getItem('SessionToken')
+
+	const response = fetch(fetch_url, {
+		method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+		  'Authorization': accessToken
+		}
+	})
+	.then(response => {
+		return response.json();
+	})
+	.then(data => {
+		var inputs = Array.prototype.slice.call(document.querySelectorAll('form input'));
+
+		Object.keys(data).map(function (dataItem){
+			console.log(data);
+			inputs.map(function (inputItem) {
+				return (inputItem.name === dataItem) ? (inputItem.value = data[dataItem]) : false;
+			  });
+		})
+	})   
+}
